b429c7591bee27f72e037fd9d71f8db0a108e073
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-01-28 22:03:54 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="calcli",
+    version="1.0",
+    packages=['calcli'],
+    entry_points={
+        "console_scripts": [
+            "calcli = calcli.__main__:main"
+        ],
+    },
+    description="A CLI calculator using argparse",
+    author="Kanak Tanwar",
+    url="https://github.com/kanakOS01/calcli.git",
+    python_requires=">=3.6"
+)
